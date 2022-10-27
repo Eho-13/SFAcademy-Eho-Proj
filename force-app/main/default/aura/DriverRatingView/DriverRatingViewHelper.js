5b9a50ca66e7f4abdf0944b01758e2a258a8be42
Eho <nrg.xiii@gmail.com>
2022-10-27 14:29:12 +0300

    Aura fix js helper

--- force-app/main/default/aura/DriverRatingView/DriverRatingViewHelper.js
+++ force-app/main/default/aura/DriverRatingView/DriverRatingViewHelper.js
@@ -2,29 +2,7 @@
 	getDriversRating : function() {
 		const action = component.get("c.getDriversRatingServerMethod");
         action.setCallback(this, function(serverResponse) {
-            const state = serverResponse.getState();
-            
-            if (state === 'SUCCESS') {
-                const drivers = serverResponse.getReturnValue();
-                component.set("v.drivers", drivers);
-                
-                console.log("drivers SUCCESS");
-            } else if (state === 'INCOMPLETE') {
-                // do smthng
-				console.log("INCOMPLETE error");
-            } else if (state === 'ERROR') {
-                const errors = serverResponse.getError();
-                if (errors) {
-                    if (errors[0] && errors[0].message) {
-                        console.log("Error message" + 
-                                    errors[0].message);
-                    } else {
-                        console.log("Unknown error");
-                    }
-                }
-                    
-            }
-                
+            component.set("v.drivers", serverResponse.getReturnValue());
         });
         
         $A.enqueueAction(action);
