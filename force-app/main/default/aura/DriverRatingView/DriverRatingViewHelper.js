6d4c64a25d69c6713a6c915ba2d68ac821c0ecac
Eho <nrg.xiii@gmail.com>
2022-10-28 17:46:08 +0300

    prettier of apex

--- force-app/main/default/aura/DriverRatingView/DriverRatingViewHelper.js
+++ force-app/main/default/aura/DriverRatingView/DriverRatingViewHelper.js
@@ -1,11 +1,10 @@
 ({
-	getDriversRating : function() {
-		const action = component.get("c.getDriversRatingServerMethod");
-        action.setCallback(this, function(serverResponse) {
-            component.set("v.drivers", serverResponse.getReturnValue());
-        });
-        
-        $A.enqueueAction(action);
-	}
-    
-})
+  getDriversRating: function (component) {
+    const action = component.get("c.getDriversRatingServerMethod");
+    action.setCallback(this, function (serverResponse) {
+      component.set("v.drivers", serverResponse.getReturnValue());
+    });
+
+    $A.enqueueAction(action);
+  }
+});
