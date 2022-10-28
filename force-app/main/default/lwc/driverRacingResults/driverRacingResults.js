b1c8e78c3b1f5b440562687ccb81130aec6a42c0
Eho <nrg.xiii@gmail.com>
2022-10-28 20:15:28 +0300

    LWC-with-errors-for-check 002

---
+++ force-app/main/default/lwc/driverRacingResults/driverRacingResults.js
@@ -0,0 +1,28 @@
+import { LightningElement, api } from 'lwc';
+import getDriverWinRaceResults from '@salesforce/apex/FormulaOneDAO.getDriverWinRaceResults';
+const columns = [
+    { label: 'Race Result Name', fieldName: 'Name',  type: 'text'  },
+    { label: 'Grand Prix Name', fieldName: 'Grand_Prix__r.Name',  type: 'text' },
+];
+
+export default class DriverRacingResults extends LightningElement {
+    @api recordId;
+    data = data;
+    columns = columns;
+
+    isError = false;
+    isLoaded = false;
+
+    async connectedCallback() {
+        try {
+            const response = await getDriverWinRaceResults({driverId : this.recordId});    
+            this.data = response;
+        } catch (error) {
+            this.isError = true;
+            console.error(error);
+        } finally {
+            this.isLoaded = true;
+        }
+    }    
+
+}
