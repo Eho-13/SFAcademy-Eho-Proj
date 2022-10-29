a00ae1e57c706d337a57489ff59fe422fc4e07a9
Eho <nrg.xiii@gmail.com>
2022-10-29 21:03:09 +0300

    PostponeRaceBatch + email notification

--- force-app/main/default/lwc/driverRacingResults/driverRacingResults.js
+++ force-app/main/default/lwc/driverRacingResults/driverRacingResults.js
@@ -1,15 +1,14 @@
 import { LightningElement, api } from 'lwc';
 import getDriverWinRaceResults from '@salesforce/apex/FormulaOneDAO.getDriverWinRaceResults';
-const columns = [
-    { label: 'Race Result Name', fieldName: 'Name',  type: 'text'  },
-    { label: 'Grand Prix Name', fieldName: 'Grand_Prix__r.Name',  type: 'text' },
-];
+const columns = [{ label: 'Race Result Name', fieldName: 'Name',  type: 'text'  },
+                 { label: 'Position', fieldName: 'Position__c',},
+                 { label: 'Grand Prix Name', fieldName: 'Grand_Prix__r.Name',  type: 'text' },
+                ];
 
 export default class driverRacingResults extends LightningElement {
     @api recordId;  //Unexpected '@'. (E024)
     data = [];      //Class properties must be methods. Expected '(' but instead saw '='. (E054)
     columns = columns;
-
     isError = false;
     isLoaded = false;
 
