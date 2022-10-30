0aa62a99f2a707c2aead3ce3c3d76bc08e7d9393
Eho <nrg.xiii@gmail.com>
2022-10-30 14:51:59 +0200

    Display current track weather Badge

---
+++ force-app/main/default/lwc/raceTrackWeather/raceTrackWeather.js
@@ -0,0 +1,36 @@
+import { LightningElement, api } from 'lwc';
+import getRaceTrackWeather from '@salesforce/apex/FormulaOneDAO.getRaceTrackWeather';
+
+export default class RaceTrackWeather extends LightningElement {
+    @api recordId;
+    weater = '';
+    isError = false;
+
+    async connectedCallback() {
+        try {
+            getRaceTrackWeather({
+                raceId: this.recordId
+            })
+            .then(response => {
+                const data = JSON.parse(response);
+                if (response.data !== '') {
+                    if(data.metric == 'fahrenheit') {
+                        const celsius = Math.round(((data.temp - 32) * 5 / 9)*100)/100;
+                        this.weater = '°C: ' + celsius;  
+                    } else {
+                        this.weater = '°c: ' + data.temp; 
+                    }
+                        
+                    console.log('RaceTrackWeather: '+data.temp);
+                } else {
+                    this.weater = 'No data';
+                    console.log("RaceTrackWeather: no data found");
+                }
+                
+            })
+        } catch (error) {
+            this.isError = true;
+            console.error(error);
+        }
+    }
+}
