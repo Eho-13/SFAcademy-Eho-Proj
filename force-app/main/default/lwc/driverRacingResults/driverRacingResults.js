6c89458e07c0525bbeb09889fc9bb89c6afaf500
Eho <nrg.xiii@gmail.com>
2022-11-03 15:50:21 +0200

    Final Prettier

--- force-app/main/default/lwc/driverRacingResults/driverRacingResults.js
+++ force-app/main/default/lwc/driverRacingResults/driverRacingResults.js
@@ -1,27 +1,29 @@
-import { LightningElement, api } from 'lwc';
-import getDriverWinRaceResults from '@salesforce/apex/FormulaOneDAO.getDriverWinRaceResults';
-const columns = [{ label: 'Race Result Name', fieldName: 'Name',  type: 'text'  },
-                 { label: 'Position', fieldName: 'Position__c',},
-                 { label: 'Grand Prix Name', fieldName: 'Grand_Prix__r.Name',  type: 'text' },
-                ];
+import { LightningElement, api } from "lwc";
+import getDriverWinRaceResults from "@salesforce/apex/FormulaOneDAO.getDriverWinRaceResults";
+const columns = [
+  { label: "Race Result Name", fieldName: "Name", type: "text" },
+  { label: "Position", fieldName: "Position__c" },
+  { label: "Grand Prix Name", fieldName: "Grand_Prix__r.Name", type: "text" }
+];
 
 export default class driverRacingResults extends LightningElement {
-    @api recordId;  //Unexpected '@'. (E024)
-    data = [];      //Class properties must be methods. Expected '(' but instead saw '='. (E054)
-    columns = columns;
-    isError = false;
-    isLoaded = false;
+  @api recordId; //Unexpected '@'. (E024)
+  data = []; //Class properties must be methods. Expected '(' but instead saw '='. (E054)
+  columns = columns;
+  isError = false;
+  isLoaded = false;
 
-    async connectedCallback() {
-        try {
-            const response = await getDriverWinRaceResults({driverId : this.recordId});  //Missing semicolon. (E058)  
-            this.data = response;
-        } catch (error) {
-            this.isError = true;
-            console.error(error);
-        } finally {
-            this.isLoaded = true;
-        }
-    }    
-
+  async connectedCallback() {
+    try {
+      const response = await getDriverWinRaceResults({
+        driverId: this.recordId
+      }); //Missing semicolon. (E058)
+      this.data = response;
+    } catch (error) {
+      this.isError = true;
+      console.error(error);
+    } finally {
+      this.isLoaded = true;
+    }
+  }
 }
--- force-app/main/default/lwc/raceTrackWeather/raceTrackWeather.js
+++ force-app/main/default/lwc/raceTrackWeather/raceTrackWeather.js
@@ -1,36 +1,34 @@
-import { LightningElement, api } from 'lwc';
-import getRaceTrackWeather from '@salesforce/apex/FormulaOneDAO.getRaceTrackWeather';
+import { LightningElement, api } from "lwc";
+import getRaceTrackWeather from "@salesforce/apex/FormulaOneDAO.getRaceTrackWeather";
 
 export default class RaceTrackWeather extends LightningElement {
-    @api recordId;
-    weater = '';
-    isError = false;
+  @api recordId;
+  weater = "";
+  isError = false;
 
-    async connectedCallback() {
-        try {
-            getRaceTrackWeather({
-                raceId: this.recordId
-            })
-            .then(response => {
-                const data = JSON.parse(response);
-                if (response.data !== '') {
-                    if(data.metric == 'fahrenheit') {
-                        const celsius = Math.round(((data.temp - 32) * 5 / 9)*10)/10;
-                        this.weater = '°C: ' + celsius;  
-                    } else {
-                        this.weater = '°c: ' + data.temp; 
-                    }
-                        
-                    console.log('RaceTrackWeather: '+data.temp);
-                } else {
-                    this.weater = 'No data';
-                    console.log("RaceTrackWeather: no data found");
-                }
-                
-            })
-        } catch (error) {
-            this.isError = true;
-            console.error(error);
+  async connectedCallback() {
+    try {
+      getRaceTrackWeather({
+        raceId: this.recordId
+      }).then((response) => {
+        const data = JSON.parse(response);
+        if (response.data !== "") {
+          if (data.metric == "fahrenheit") {
+            const celsius = Math.round((((data.temp - 32) * 5) / 9) * 10) / 10;
+            this.weater = "°C: " + celsius;
+          } else {
+            this.weater = "°c: " + data.temp;
+          }
+
+          console.log("RaceTrackWeather: " + data.temp);
+        } else {
+          this.weater = "No data";
+          console.log("RaceTrackWeather: no data found");
         }
+      });
+    } catch (error) {
+      this.isError = true;
+      console.error(error);
     }
+  }
 }
